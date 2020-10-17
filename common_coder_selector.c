e8cd6414970951176989ff30330c427033b25e80
azulzaietz <azulzaietz@gmail.com>
2020-10-17 00:49:14 -0300

    coder selector

---
+++ common_coder_selector.c
@@ -0,0 +1,37 @@
+#include "common_coder_selector.h"
+
+void coder_selector_init(coder_selector_t* self, const char* method, const char* key) {
+    self->key = key;
+    _select_coder_method(self, method);
+}
+
+void _select_coder_method(coder_selector_t* self, const char* method) {
+    
+    char* method_cesar = "cesar";
+    char* method_vigenere = "vigenere";
+    char* method_rc4 = "rc4";
+
+    if (strcmp(method, method_cesar) == 0) {
+        
+        cesar_t* cesar;
+        self->coder = cesar;
+        /*cesar_create(self->coder, self->key);
+        self->coder_t = cesar_code;*/
+
+    } if (strcmp(method, method_vigenere) == 0) {
+
+        vigenere_t* vigenere;
+        self->coder = vigenere;
+        /*vigenere_create(self->coder, self->key);
+        self->coder_t = vigenere_code;*/
+
+    } if (strcmp(method, method_rc4) == 0) {
+ 
+        rc4_t* rc4;
+        self->coder = rc4;
+        /*rc4_create(self->coder, self->key);
+        self->coder_t = rc4_code;*/
+
+    }
+
+}
