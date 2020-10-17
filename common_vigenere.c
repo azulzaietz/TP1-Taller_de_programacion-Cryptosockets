42966b0c740d45eee515725b92d1248058224ac5
azulzaietz <azulzaietz@gmail.com>
2020-10-17 09:49:26 -0300

    key unsigned char

--- common_vigenere.c
+++ common_vigenere.c
@@ -1,18 +1,18 @@
 #include "common_vigenere.h"
 
 int vigenere_create(vigenere_t* self, const char* key) {
-    self->key = key;
+    self->key = (unsigned char*) key;
     return 0;
 }
 
 void vigenere_code(vigenere_t* self, unsigned char *message, size_t read_bytes) {
     for (int i = 0; i < read_bytes; i++){
-        message[i] = (message[i] + (unsigned char) self->key[i % strlen(self->key)]) % 256;
+        message[i] = (message[i] + self->key[i % strlen(self->key)]) % 256;
     }
 }
 
 void vigenere_decode(vigenere_t* self, unsigned char* message, size_t read_bytes) {
     for (int i = 0; i < read_bytes; i++){
-        message[i] = (message[i] - (unsigned char) self->key[i % strlen(self->key)]) % 256;
+        message[i] = (message[i] - self->key[i % strlen(self->key)]) % 256;
     }
 }
