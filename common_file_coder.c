70e42d200c07bda1c9756530064d348dc38d57b3
azulzaietz <azulzaietz@gmail.com>
2020-10-17 00:49:39 -0300

    delega selección del coder

--- common_file_coder.c
+++ common_file_coder.c
@@ -1,50 +1,18 @@
 #include "common_file_coder.h"
 
-int file_coder_init(file_coder_t* self, const char* file_name, const char* method, const char* key) {
+int file_coder_init(file_coder_t* self, const char* file_name, coder_selector_t* coder_selector) {
 
     if (file_name == NULL) self->fp = stdin;
     else self->fp = fopen(file_name, "rb");
 
-/*
     if(!self->fp) {
 //        printf(stderr, "file_reader_init-->open %s\n", strerror(errno));
         return -1;
-    }*/
-
-    _init_coder_method(self, method, key);
-
-    return 0;
-}
-
-void _init_coder_method(file_coder_t* self, const char* method, const char* key) {
-    
-    char* method_cesar = "cesar";
-    char* method_vigenere = "vigenere";
-    char* method_rc4 = "rc4";
-
-    if (strcmp(method, method_cesar) == 0) {
-        
-        cesar_t* cesar;
-        self->coder = cesar;
-        cesar_create(self->coder, key);
-        coder_t = &cesar_code;
-
-    } if (strcmp(method, method_vigenere) == 0) {
-
-        vigenere_t* vigenere;
-        self->coder = vigenere;
-        vigenere_create(self->coder, key);
-        coder_t = &vigenere_code;
-
-    } if (strcmp(method, method_rc4) == 0) {
- 
-        rc4_t* rc4;
-        self->coder = rc4;
-        rc4_create(self->coder, key);
-        coder_t = &rc4_code;
-
     }
 
+    self->coder_selector = coder_selector;
+
+    return 0;
 }
 
 int file_coder_uninit(file_coder_t* self) {
@@ -57,14 +25,31 @@ int file_coder_uninit(file_coder_t* self) {
     return 0;
 }
 
-int code_file(file_coder_t* self, file_coder_callback_t callback, void* callback_ctx) {
+int code_file(file_coder_t* self, socket_t* socket) {
 
-    char buffer[BUF_SIZE];
+    unsigned char buffer[BUF_SIZE];
 
     while(!feof(self->fp)){
+
         size_t read_bytes = fread(buffer, sizeof(char), BUF_SIZE, self->fp);
-        //coder_t(self->coder, buffer, read_bytes);
-        callback(buffer, read_bytes, callback_ctx);
+        self->coder_selector->coder_t(self->coder_selector->coder, buffer, read_bytes);
+        socket_send(socket, buffer, read_bytes);
     }
     return 0;
 }
+
+
+ /*
+        if (strcmp(self->method, method_cesar) == 0) {
+        
+	        cesar_code(self->coder, buffer, read_bytes);
+
+	    } if (strcmp(self->method, method_vigenere) == 0) {
+
+	        vigenere_code(self->coder, buffer, read_bytes);
+
+	    } if (strcmp(self->method, method_rc4) == 0) {
+	 
+	        rc4_code(self->coder, buffer, read_bytes);
+
+	    }*/
--- common_file_coder.h
+++ common_file_coder.h
@@ -1,6 +1,5 @@
-#include "common_cesar.h"
-#include "common_vigenere.h"
-#include "common_RC4.h"
+#include "common_coder_selector.h"
+#include "common_socket.h"
 
 #include <stdio.h>
 #include <stddef.h>
@@ -13,22 +12,21 @@
  *                DEFINICION DE LOS TIPOS DE DATOS
  * *****************************************************************/
 
-typedef void (*file_coder_callback_t)(char *chunk, size_t chunk_size, void *callback_ctx);
+//typedef void (*file_coder_callback_t)(char *chunk, size_t chunk_size, void *callback_ctx);
 
 typedef struct {
     FILE* fp;
-    void* coder;
+    coder_selector_t* coder_selector;
 } file_coder_t;
 
 /* *****************************************************************
  *                PRIMITIVAS DEL FILE CODER
  * *****************************************************************/
 
-int file_coder_init(file_coder_t* self, const char* file_name, const char* method, const char* key);
+int file_coder_init(file_coder_t* self, const char* file_name, coder_selector_t* coder);
 
-void (*coder_t) (void* coder, unsigned char* message, size_t length);
 void _init_coder_method(file_coder_t* self, const char* method, const char* key);
 
-int code_file(file_coder_t* self, file_coder_callback_t callback, void* callback_ctx);
+int code_file(file_coder_t* self, socket_t* socket);
 
 int file_coder_uninit(file_coder_t* self);
