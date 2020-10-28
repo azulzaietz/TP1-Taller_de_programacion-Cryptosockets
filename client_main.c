8c5431af9d5eabc29fc479f08dc63170e8a8175e
azulzaietz <azulzaietz@gmail.com>
2020-10-27 22:40:28 -0300

    valida cantidad de argumentos

--- client_main.c
+++ client_main.c
@@ -2,6 +2,7 @@
 
 int main(int argc, char* const* argv) {
     client_t client;
+    if (argc != 5) return -1;
     init_client(&client, argc, argv);
     send_coded_message_to_server(&client);
 
--- server_main.c
+++ server_main.c
@@ -2,6 +2,7 @@
 
 int main(int argc, char* const* argv) {
     server_t server;
+    if (argc != 4) return -1;
     server_init(&server, argc, argv);
     receive_coded_message_from_client(&server);
 
